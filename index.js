b147c9ce0e7788560a0bc0e2208722454bbf443e
RainBow <1656800216@qq.com>
2019-04-28 11:19:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 'use strict';
 
+/**
+ * 为数组添加新的自定义键值以及过滤每个子项的方法
+ *
+ * @param {*} arr
+ * @param {*} obj { isShow:false,isStar:false} 第二个参数为 Function 时
+ * @param {*} filterFn 第二个参数为 Object 时
+ * @returns Array
+ */
+
 module.exports = function addKey(sourceArray, extendObj, filterFn) {
-    if (!Array.isArray(sourceArray)) {
-        throw new Error("第一个参数必须为数组类型")
-    }
+  var getType = function (a) {
+    var typeArray = Object.prototype.toString.call(a).split(" ");
+    return typeArray[1].slice(0, -1);
+  }
+  var secondParamType = getType(arguments[1]);
+
+  if (!getType(sourceArray) == 'Array') {
+    throw new Error("第一个参数必须为数组类型")
+  }
+  if (secondParamType === 'Object') {
     return sourceArray.forEach((v, index, sourceArray) => {
-        for (var key in extendObj) {
-            v[key] = extendObj[key]
-        }
-        typeof filterFn === 'function' ? filterFn(v, index, sourceArray) : '';
+      for (var key in extendObj) {
+        v[key] = extendObj[key]
+      }
+      typeof filterFn === 'function' ? filterFn(v, index, sourceArray) : '';
     })
+  } else if (secondParamType === 'Function') {
+    return sourceArray.forEach((v, index, sourceArray) => {
+      typeof arguments[1] === 'function' ? arguments[1](v, index, sourceArray) : '';
+    })
+  } else {
+    return sourceArray;
+  }
 }
