6ba86f32e0fa6b80641c8d115267846ded83fcd6
xiaoyueyue165 <1656800216@qq.com>
2019-05-07 21:54:50 +0800

    add .editorconfig

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # addKey [![NPM version](https://img.shields.io/npm/v/addkey.svg?style=flat)](https://www.npmjs.com/package/addkey) [![NPM monthly downloads](https://img.shields.io/npm/dm/addkey.svg?style=flat)](https://npmjs.org/package/addkey) [![NPM total downloads](https://img.shields.io/npm/dt/addkey.svg?style=flat)](https://npmjs.org/package/addkey) [![Build Status](https://travis-ci.org/xiaoyueyue165/addKey.svg?branch=master)](https://travis-ci.org/xiaoyueyue165/addKey)
-> Add a new key-value pair for each single item of the array type and filter the children
 
+> Add a new key-value pair for each single item of the array type and filter the children
 
 ## Usage
 
@@ -19,24 +19,24 @@ $ npm install --save addkey
 Works with arrays.
 
 ```js
-const addKey = require('addkey');
+const addKey = require("addkey");
 let arr = [
-    {
-        id: 1,
-        name: '亚瑟'
-    },
-    {
-        id: 2,
-        name: '狄仁杰'
-    },
-    {
-        id: 3,
-        name: '曹操'
-    }
-]
- addKey(arr, { 'isShow': false }, (v, index, array) => {
-    index === array.length - 1 ? (v.name = '铠', v.isShow = true) : ''
-})
+  {
+    id: 1,
+    name: "亚瑟"
+  },
+  {
+    id: 2,
+    name: "狄仁杰"
+  },
+  {
+    id: 3,
+    name: "曹操"
+  }
+];
+addKey(arr, { isShow: false }, (v, index, array) => {
+  index === array.length - 1 ? ((v.name = "铠"), (v.isShow = true)) : "";
+});
 
 // log arr =>
 
@@ -49,9 +49,7 @@ let arr = [
 */
 ```
 
-
 ### License
 
 Copyright © 2019, [xiaoyueyue](https://github.com/xiaoyueyue165).
 Released under the [MIT License](LICENSE).
-
--- test.js
+++ test.js
@@ -1,50 +1,48 @@
-'use strict';
-require('mocha');
-const assert = require('assert');
-const addKey = require('./');
+"use strict";
+require("mocha");
+const assert = require("assert");
+const addKey = require("./");
 
 let arr = [
-    {
-        id: 1,
-        name: '亚瑟'
-    },
-    {
-        id: 2,
-        name: '狄仁杰'
-    },
-    {
-        id: 3,
-        name: '曹操'
-    }
-]
-describe('addKey', function () {
-    it('should return add custom key:', function () {
-        addKey(arr, { 'Address': 'Chinese' })
+  {
+    id: 1,
+    name: "亚瑟"
+  },
+  {
+    id: 2,
+    name: "狄仁杰"
+  },
+  {
+    id: 3,
+    name: "曹操"
+  }
+];
+describe("addKey", function() {
+  it("should return add custom key:", function() {
+    addKey(arr, { Address: "Chinese" });
 
-        assert(arr[0].Address === 'Chinese')
-        assert(arr[1].Address === 'Chinese')
-        assert(arr[2].Address === 'Chinese')
+    assert(arr[0].Address === "Chinese");
+    assert(arr[1].Address === "Chinese");
+    assert(arr[2].Address === "Chinese");
+  });
+
+  it("should return add custom key and filtered correctly:", function() {
+    addKey(arr, { isShow: false }, (v, index, array) => {
+      index === array.length - 1 ? ((v.name = "铠"), (v.isShow = true)) : "";
     });
 
-    it('should return add custom key and filtered correctly:', function () {
-        addKey(arr, { 'isShow': false }, (v, index, array) => {
-            index === array.length - 1 ? (v.name = '铠', v.isShow = true) : ''
-        })
+    assert(!arr[0].isShow);
+    assert(!arr[1].isShow);
+    assert(arr[2].isShow); // true
+  });
 
-        assert(!arr[0].isShow)
-        assert(!arr[1].isShow)
-        assert(arr[2].isShow) // true
+  it("should return omit custom key and filtered correctly:", function() {
+    addKey(arr, (v, index, array) => {
+      index === array.length - 1 ? ((v.name = "曹操"), (v.isShow = true)) : "";
     });
 
-    it('should return omit custom key and filtered correctly:', function () {
-        addKey(arr, (v, index, array) => {
-            index === array.length - 1 ? (v.name = '曹操', v.isShow = true) : ''
-        })
-
-        assert(!arr[0].isShow)
-        assert(!arr[1].isShow)
-        assert(arr[2].isShow) // true
-    });
-
-
+    assert(!arr[0].isShow);
+    assert(!arr[1].isShow);
+    assert(arr[2].isShow); // true
+  });
 });
